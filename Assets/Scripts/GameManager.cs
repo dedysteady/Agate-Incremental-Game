80155e5b9752f3ea25553650a15e85e401ee1376
dedysteady <dedysetiadi1708@gmail.com>
2021-09-12 09:54:23 +0700

    adding tap collect

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,72 +1,41 @@
 ﻿using System.Collections.Generic;
-
 using UnityEngine;
-
 using UnityEngine.UI;
 
-
-
 public class GameManager : MonoBehaviour
-
 {
-
     private static GameManager _instance = null;
-
     public static GameManager Instance
-
     {
-
         get
-
         {
-
             if (_instance == null)
-
             {
-
                 _instance = FindObjectOfType<GameManager>();
-
             }
-
-
-
             return _instance;
-
         }
-
     }
 
 
-
     // Fungsi [Range (min, max)] ialah menjaga value agar tetap berada di antara min dan max-nya 
-
     [Range(0f, 1f)]
-
     public float AutoCollectPercentage = 0.1f;
-
     public ResourceConfig[] ResourcesConfigs;
-
-
-
     public Transform ResourcesParent;
-
     public ResourceController ResourcePrefab;
-
-
-
+    public TapText TapTextPrefab;
+    public Transform CoinIcon;
     public Text GoldInfo;
-
     public Text AutoCollectInfo;
 
-
-
     private List<ResourceController> _activeResources = new List<ResourceController>();
-
+    private List<TapText> _tapTextPool = new List<TapText> ();
     private float _collectSecond;
 
 
 
-    private double _totalGold;
+    public double TotalGold { get; private set; }
 
 
 
@@ -97,6 +66,10 @@ public class GameManager : MonoBehaviour
             _collectSecond = 0f;
 
         }
+
+        CoinIcon.transform.localScale = Vector3.LerpUnclamped (CoinIcon.transform.localScale, Vector3.one * 2f, 0.15f);
+
+        CoinIcon.transform.Rotate (0f, 0f, Time.deltaTime * -100f);
 
     }
 
@@ -160,9 +133,69 @@ public class GameManager : MonoBehaviour
 
     {
 
-        _totalGold += value;
+        TotalGold += value;
 
-        GoldInfo.text = $"Gold: { _totalGold.ToString("0") }";
+        GoldInfo.text = $"Gold: { TotalGold.ToString("0") }";
+
+    }
+
+    public void CollectByTap (Vector3 tapPosition, Transform parent)
+
+    {
+
+        double output = 0;
+
+        foreach (ResourceController resource in _activeResources)
+
+        {
+
+            output += resource.GetOutput ();
+
+        }
+
+ 
+
+        TapText tapText = GetOrCreateTapText ();
+
+        tapText.transform.SetParent (parent, false);
+
+        tapText.transform.position = tapPosition;
+
+ 
+
+        tapText.Text.text = $"+{ output.ToString ("0") }";
+
+        tapText.gameObject.SetActive (true);
+
+        CoinIcon.transform.localScale = Vector3.one * 1.75f;
+
+ 
+
+        AddGold (output);
+
+    }
+
+ 
+
+    private TapText GetOrCreateTapText ()
+
+    {
+
+        TapText tapText = _tapTextPool.Find (t => !t.gameObject.activeSelf);
+
+        if (tapText == null)
+
+        {
+
+            tapText = Instantiate (TapTextPrefab).GetComponent<TapText> ();
+
+            _tapTextPool.Add (tapText);
+
+        }
+
+ 
+
+        return tapText;
 
     }
 
@@ -170,17 +203,10 @@ public class GameManager : MonoBehaviour
 
 
 [System.Serializable]
-
 public struct ResourceConfig
-
 {
-
     public string Name;
-
     public double UnlockCost;
-
     public double UpgradeCost;
-
     public double Output;
-
 }
